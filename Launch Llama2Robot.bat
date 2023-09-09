96c3cd0999ed42de02ea393883b8ed6e7b901f82
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2023-09-09 02:39:18 +0100

    moved window batches to scripts.

--- Launch Llama2Robot.bat
+++ Launch Llama2Robot.bat
@@ -1,6 +1,5 @@
 @echo off
 mode 45,20
-rem con:cols=90 lines=100
 title Llama2Robot-Launcher
 
 :: Run both windows for Llama2Robot
@@ -9,8 +8,8 @@ echo.
 echo Options for main1: --output
 echo.
 @echo on
-start cmd.exe /K "call main1.bat --output"
-start cmd.exe /K "call main2.bat"
+start cmd.exe /K "call .\scripts\window1.bat --output"
+start cmd.exe /K "call .\scripts\window2.bat"
 echo Window1 & Windows2, Launched!
 @echo off
 echo.
