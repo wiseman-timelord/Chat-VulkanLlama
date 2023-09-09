19fe1dd7e2e2ff98990753aeb931fc85d10b6bf3
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2023-09-09 15:43:20 +0100

    Add files via upload

--- Launch Llama2Robot.bat
+++ Launch Llama2Robot.bat
@@ -2,19 +2,51 @@
 mode 45,20
 title Llama2Robot-Launcher
 
-:: Run both windows for Llama2Robot
-echo Launching, Window1 & Windows2...
+:: Intro
 echo.
+echo.
+echo The Llama2Robot-Launcher
+echo -=-=-=-=-=-=-=-=-=-=-=-=
+echo.
+
+:: Check for ENV file
+echo.
+echo Searching for ENV...
+timeout /t 1 /nobreak >nul
+if exist .\.ENV (
+    echo ENV file found.
+    timeout /t 2 /nobreak >nul
+) else (
+    echo ...ENV file not found...
+    timeout /t 1 /nobreak >nul
+    echo.
+    copy .\data\example.ENV .\.ENV
+    echo ...ENV file created.
+    echo.
+    echo Review .ENV now then re-launch!
+    echo.
+    timeout /t 2 /nobreak >nul
+    echo.
+    pause
+    exit /b
+)
+
+
+:: Run both windows for Llama2Robot
+echo Launching, Window1 & Window2...
+echo.
+timeout /t 1 /nobreak >nul
 echo Options for main1: --output
+timeout /t 2 /nobreak >nul
 echo.
 @echo on
 start cmd.exe /K "call .\scripts\window1.bat --output"
 start cmd.exe /K "call .\scripts\window2.bat"
-echo Window1 & Windows2, Launched!
+echo Window1 & Window2, Launched!
 @echo off
+timeout /t 1 /nobreak >nul
 echo.
-echo.
-
-:: Exiting
 echo Exiting Launcher!
+echo.
 exit
+
