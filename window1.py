cfdefda64754346d55602951e3587314fc77fa3d
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2023-09-14 16:05:13 +0100

    resizing and title, in main scripts, not batch.

---
+++ window1.py
@@ -0,0 +1,136 @@
+# main1.py
+
+# imports
+from scripts import interface, model as model_module, utility
+import time
+import argparse
+import sys
+import os
+import readline
+
+# globals
+parser = argparse.ArgumentParser(description='Your script description here.')
+parser.add_argument('--logs', action='store_true', help='Enable writing of raw output to logs')
+args = parser.parse_args()
+loaded_models = {}
+
+# Set window title and size for Linux
+sys.stdout.write("\x1b]2;Llama2Robot-Window1\x07")
+sys.stdout.flush()
+os.system('echo -e "\e[8;45;90t"')
+
+# Handle 'reset' input
+def handle_reset():
+    utility.reset_keys_to_empty()
+    human_name, model_name, model_role, scenario_location, model_emotion, session_history = interface.roleplay_configuration()
+    yaml_data = {
+        'human_name': human_name,
+        'model_name': model_name,        
+        'model_role': model_role,
+        'scenario_location': scenario_location,
+        'model_emotion': model_emotion,
+        'session_history': session_history,
+        'human_current': "Empty",
+        'model_current': "Empty",
+        'model_previous1': "Empty",
+        'model_previous2': "Empty",
+        'sound_event': "None" 
+    }
+    for key, value in yaml_data.items():
+        utility.write_to_yaml(key, value)
+
+# Handle 'quit' input
+def handle_quit():
+    print("\n Quitting the script. Goodbye!")
+    time.sleep(2)
+    sys.exit(0)
+
+# Handle 'other' input
+def handle_other(user_input, rotation_counter, loaded_models):
+    data = utility.read_yaml()
+    if 'model_previous1' not in data:
+        print("Error: 'model_previous1' key not found in YAML data.")
+        return
+    human_name = data.get('human_name')
+    model_name = data.get('model_name')
+    model_role = data.get('model_role')
+    model_emotion = data.get('model_emotion')
+    scenario_location = data.get('scenario_location')
+    session_history = data.get('session_history')
+    human_current = data.get('human_current') 
+    model_current = data.get('model_current')
+    model_previous1 = data.get('model_previous1')   
+    model_previous2 = data.get('model_previous2')      
+    start_time = time.time()
+    current_task = 'emotions' if rotation_counter == 3 else 'converse'
+    response_dict = model_module.prompt_response(current_task, rotation_counter, enable_logging=args.logs, loaded_models=loaded_models, save_to='model_current')
+    consolidate_dict = model_module.prompt_response('consolidate', rotation_counter, enable_logging=args.logs, loaded_models=loaded_models, save_to='session_history')
+    if args.logs:
+        utility.write_to_yaml('raw_output', response_dict['model_response'])
+    new_session_history = consolidate_dict.get('new_session_history')
+    if new_session_history:
+        utility.write_to_yaml('session_history', new_session_history)
+    if rotation_counter == 3:
+        new_emotion = response_dict.get('new_emotion')
+        if new_emotion:
+            utility.write_to_yaml('model_emotion', new_emotion)
+    end_time = time.time()
+    print(f"\n ...Time taken: {end_time - start_time:.2f} seconds...")
+    utility.shift_responses()
+    print(" ...Key Display window updated.\n")
+
+# Main function
+def main():
+    try:
+        optimal_threads = interface.display_intro_screen()
+        time.sleep(1)
+        utility.reset_keys_to_empty()
+        selected_models = interface.display_model_selection()
+        if not selected_models:
+            print("No Models, Exiting!")
+            return
+        loaded_models = {}
+        for model_type in ['chat', 'instruct']:
+            model = selected_models.get(model_type)
+            context_key = selected_models.get(model_type + '_context')
+            if model and model not in loaded_models.values():
+                model_module.initialize_model(model, optimal_threads, model_type=model_type, context_key=context_key)
+                loaded_models[model_type] = model
+        human_name, model_name, model_role, model_emotion, scenario_location, session_history = interface.roleplay_configuration()
+        yaml_data = {
+            'human_name': human_name,
+            'model_name': model_name,
+            'model_role': model_role,
+            'model_emotion': model_emotion,
+            'scenario_location': scenario_location,
+            'session_history': session_history,
+            'sound_event': "None" 
+        }
+        for key, value in yaml_data.items():
+            utility.write_to_yaml(key, value)
+        data = utility.read_yaml()
+        human_name = data.get('human_name')
+        model_name = data.get('model_name')
+        model_emotion = data.get('model_emotion')
+        session_history = data.get('session_history')
+        human_current = data.get('human_current')
+        model_current = data.get('model_current') 
+        rotation_counter = 0
+        while True:
+            interface.display_engine()  
+            user_input = input(f" Enter your message to {model_name} or 'reset' to Restart or 'quit' to Exit?:\n").lower()
+            if user_input == 'reset':
+                handle_reset()
+            elif user_input == 'quit':
+                handle_quit()
+            else:
+                readline.add_history(user_input)
+                utility.write_to_yaml('human_current', user_input)
+                handle_other(user_input, rotation_counter, loaded_models)
+            rotation_counter = (rotation_counter + 1) % 4
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Call the main function
+if __name__ == "__main__":
+    main()
---
+++ window2.py
@@ -0,0 +1,179 @@
+# main2.py
+
+# Imports
+from watchdog.observers import Observer
+from watchdog.observers.polling import PollingObserver
+from watchdog.events import FileSystemEventHandler
+import sounddevice as sd
+import yaml
+import time
+import os
+import sys
+import argparse
+import platform
+import pyttsx3
+import wavio
+
+# General Variables
+parser = argparse.ArgumentParser(description='Your Project Description')
+parser.add_argument('--tts', action='store_true', help='Enable text-to-speech')
+parser.add_argument('--sound', action='store_true', help='Enable sounds')
+args = parser.parse_args()
+os_name = platform.system()
+last_session_history = None
+last_sound_event = None
+SOUND_DIRECTORY = "./data/sounds"
+
+# Detect OS and set window size and title accordingly
+os_name = platform.system()
+if os_name == 'Windows':
+    os.system('mode con: cols=90 lines=45')
+    os.system('title Llama2Robot-Window2')
+elif os_name == 'Linux':
+    sys.stdout.write("\x1b]2;Llama2Robot-Window2\x07")
+    sys.stdout.flush()
+    os.system('echo -e "\e[8;45;90t"')
+
+# TTS Variables
+TTS_RATE = 150  
+TTS_VOLUME = 0.9  
+TTS_VOICE_ID = 1
+tts_counter = 0  
+
+# Class
+class Watcher:
+    DIRECTORY_TO_WATCH = "./data"
+    def __init__(self):
+        self.observer = PollingObserver()
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except:
+            self.observer.stop()
+            print("Observer stopped")
+        self.observer.join()
+
+# Class
+class Handler(FileSystemEventHandler):
+    @staticmethod
+    def process(event):
+        global last_session_history, last_sound_event
+        should_update_display = False  # Flag to control display and sound updates
+        if event.src_path.endswith('config.yaml'):
+            data = read_yaml()
+            current_sound_event = data.get('sound_event', '')
+            if current_sound_event != last_sound_event:
+                last_sound_event = current_sound_event
+                sound_file = f"{SOUND_DIRECTORY}/{current_sound_event}.wav"
+                if os.path.exists(sound_file) and args.sound:
+                    play_wav(sound_file)
+            current_session_history = data.get('session_history', '')
+            if current_session_history != last_session_history:
+                last_session_history = current_session_history
+                should_update_display = True  # Set flag to True if session_history is updated
+                
+            if should_update_display:  # Check the flag before updating display and sound
+                print(" ...changes detected, re-printing Display...\n")
+                if args.sound:
+                    play_wav(f"{SOUND_DIRECTORY}/change_detect.wav")  
+                time.sleep(1)
+                fancy_delay(5)
+                display_interface()
+                
+                if args.tts and os_name == 'Windows':  # Moved this block here
+                    speak_text(current_session_history)
+
+    def on_modified(self, event):
+        self.process(event)
+
+# Fancy loading bar
+def fancy_delay(duration, message=" Loading..."):
+    step = duration / 100
+    sys.stdout.write(f"{message} [")
+    for _ in range(64):
+        time.sleep(step)
+        sys.stdout.write("=")
+        sys.stdout.flush()
+    sys.stdout.write("] Complete.\n")
+    time.sleep(1)
+
+# Read the config.yaml        
+def read_yaml(file_path='./data/config.yaml'):
+    try:
+        with open(file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except FileNotFoundError:
+        print("Error: config.yaml not found.")
+        return None
+    except Exception as e:
+        print(f"An error occurred while reading config.yaml: {e}")
+        return None
+
+# Text-to-Speech Function
+def speak_text(text):
+    global tts_counter
+    tts_counter += 1
+    if not args.tts or tts_counter <= 1:
+        return
+    if os_name == 'Windows':
+        engine = pyttsx3.init()
+        engine.setProperty('rate', TTS_RATE)
+        engine.setProperty('volume', TTS_VOLUME)
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[TTS_VOICE_ID].id)
+        engine.say(text)
+        engine.runAndWait()
+    elif os_name == 'Linux':
+        print("Linux TTS is not yet implemented.")
+    else:
+        print("Unsupported OS for TTS.")
+
+# Play the sample
+def play_wav(filename):
+    if args.sound:
+        wav = wavio.read(filename)
+        sd.play(wav.data, wav.rate)
+        sd.wait()
+
+# main display
+def display_interface():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    data = read_yaml()
+    if data is None:
+        return
+    human_name = data.get('human_name')
+    model_name = data.get('model_name')
+    model_current = data.get('model_current')
+    model_emotion = data.get('model_emotion')
+    session_history = data.get('session_history')
+    human_current = data.get('human_current')
+    print("=" * 90)
+    print("                                     ROLEPLAY SUMMARY")
+    print("=" * 90, "=-" * 44)
+    print(f" {human_name}'s Input:")
+    print("-" * 90)
+    print(f"\n {human_current}\n")
+    print("=-" * 45, "=-" * 44)
+    print(f" {model_name}'s Response:")
+    print("-" * 90)
+    print(f"\n {model_current}\n")
+    print("=-" * 45, "=-" * 44)    
+    print(f" {model_name}'s State:")
+    print("-" * 90)
+    print(f"\n {model_emotion}\n")  
+    print("=-" * 45, "=-" * 44)   
+    print(" Event History:")
+    print("-" * 90)
+    print(f"\n {session_history}\n")
+    print("=-" * 45)
+    print("\n Listening for changes to config.yaml...")
+
+# End bit
+if __name__ == '__main__':
+    display_interface()
+    w = Watcher()
+    w.run()
